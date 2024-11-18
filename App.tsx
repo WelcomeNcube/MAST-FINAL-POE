2292f8da6f9aac6a82b87b37a24e40e90c00c5cc
WelcomeNcube <ST10445792@imconnect.edu.za>
2024-11-19 00:18:33 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AddMenuScreen from './screens/AddMenuScreen';
+import FilterScreen from './screens/FilterScreen';
+
+export type MenuItem = {
+  name: string;
+  description: string;
+  course: string;
+  price: string;
+};
+
+export type RootStackParamList = {
+  Home: undefined;
+  AddMenu: undefined;
+  Filter: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+
+  const addMenuItem = (item: MenuItem) => {
+    setMenuItems([...menuItems, item]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home">
+          {(props) => <HomeScreen {...props} menuItems={menuItems} setMenuItems={setMenuItems} />}
+        </Stack.Screen>
+        <Stack.Screen name="AddMenu">
+          {(props) => <AddMenuScreen {...props} addMenuItem={addMenuItem} />}
+        </Stack.Screen>
+        <Stack.Screen name="Filter">
+          {(props) => <FilterScreen {...props} menuItems={menuItems} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
